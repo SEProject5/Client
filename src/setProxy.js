63e9abdf77ce631577c3a009f14c0b213f32371e
highcastle4242 <wnstjd4242@naver.com>
2021-06-03 06:09:10 +0900

    modify: proxy

--- src/setProxy.js
+++ src/setProxy.js
@@ -3,7 +3,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/', {
-      target: 'https://kapi.kakao.com',
+      target:
+        'http://ec2-13-125-128-80.ap-northeast-2.compute.amazonaws.com:3001',
       changeOrigin: true,
     })
   );
